e71aa74d5f499779f6d14a617bbfad8a3a2946b0
Mikhail141 <70147484+Mikhail141@users.noreply.github.com>
2020-11-05 13:59:53 +0300

    test

--- src/main/java/ru/netology/manager/ProductManager.java
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -7,23 +7,11 @@ import ru.netology.repository.ProductRepository;
 
 public class ProductManager {
 
-    private Product[] items = new Product[0];
-
     private ProductRepository repository;
-
-
-    public ProductManager() {
-    }
 
     public ProductManager(ProductRepository repository) {
         this.repository = repository;
     }
-
-    public ProductManager(Product[] items, ProductRepository repository) {
-        this.items = items;
-        this.repository = repository;
-    }
-
     public void add(Product item) {
         repository.save(item);
     }
@@ -34,7 +22,7 @@ public class ProductManager {
         for (Product product: repository.findAll()) {
             if (matches(product, text)) {
                 Product[] tmp = new Product[result.length + 1];
-                System.arraycopy(result, 0, tmp, 0, items.length);
+                System.arraycopy(result, 0, tmp, 0,result.length);
                 tmp[tmp.length - 1] = product;
                 result = tmp;
             }
--- src/test/java/ru/netology/domain/ProductManagerTest.java
+++ src/test/java/ru/netology/domain/ProductManagerTest.java
@@ -9,23 +9,23 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 
 public class ProductManagerTest {
-
-
-    private ProductRepository repository;
-
-    private ProductManager manager;
-
+    private ProductRepository repository = new ProductRepository();
+    ProductManager manager = new ProductManager(repository);
     Product f = new Product(1,"f",1);
     Product s = new Product(1,"s",1);
 
-    @Test
-    public void addBook(){
-        ProductManager manager = new ProductManager();
 
+
+
+    @Test
+    public void shouldsearchName() {
+        String search = "f";
         manager.add(f);
-        Product[] actual = manager.getAll(f);
+        Product[] actual = manager.searcyBy(search);
         Product[] expected = new Product[]{f};
-        assertArrayEquals(expected, actual);
-    }
-    }
+        assertArrayEquals(expected,actual);
+}
+}
+
+
 
