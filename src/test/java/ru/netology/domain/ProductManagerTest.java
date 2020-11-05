82fa1d6b9a56f56af8e35f4d42171df6dd1094b7
Mikhail141 <70147484+Mikhail141@users.noreply.github.com>
2020-11-05 10:14:32 +0300

    method

---
+++ src/test/java/ru/netology/domain/ProductManagerTest.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.manager.ProductManager;
+import ru.netology.repository.ProductRepository;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class ProductManagerTest {
+    Product f = new Product(1,"f",1);
+    Product s = new Product(1,"s",1);
+
+    @Test
+    public void addBook(){}
+}
